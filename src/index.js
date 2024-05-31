bc7a145a3f9ce01ff41c90c36fc9f8d9851663bd
roman-ruzheynikov <rrd2193@gmail.com>
2024-05-31 12:07:25 +0300

    v2.0.4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 //const { updateElectronApp, UpdateSourceType } = require('update-electron-app');
 //const { autoUpdater } = require("electron-updater");
-const { autoUpdater } = require('update-electron-app')();
+const { autoUpdater } = require('update-electron-app');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 // if (require('electron-squirrel-startup')) {
